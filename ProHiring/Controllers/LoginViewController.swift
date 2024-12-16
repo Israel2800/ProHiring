f7b23ef15b678ffef87287c6893c092eab8745ba
Israel2800 <dady281100@gmail.com>
2024-12-16 14:00:29 -0600

    Working on the interaces

---
+++ ProHiring/Controllers/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  ProHiring
+//
+//  Created by Paola Delgadillo on 12/16/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController{
+    
+    
+}
