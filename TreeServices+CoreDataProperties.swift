8a6b323f19649602bb873df78b95122c90959174
Israel2800 <dady281100@gmail.com>
2024-12-23 02:26:59 -0600

    Fixing api issues

--- TreeServices+CoreDataProperties.swift
+++ TreeServices+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  TreeServices+CoreDataProperties.swift
 //  ProHiring
 //
-//  Created by Paola Delgadillo on 12/19/24.
+//  Created by Paola Delgadillo on 12/23/24.
 //
 //
 
@@ -16,12 +16,12 @@ extension TreeServices {
         return NSFetchRequest<TreeServices>(entityName: "TreeServices")
     }
 
+    @NSManaged public var descrip: String?
+    @NSManaged public var duration: String?
     @NSManaged public var id: String?
+    @NSManaged public var price: String?
     @NSManaged public var thumbnail: String?
     @NSManaged public var title: String?
-    @NSManaged public var descrip: String?
-    @NSManaged public var price: String?
-    @NSManaged public var duration: String?
 
 }
 
